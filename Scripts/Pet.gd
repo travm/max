795717bc5087ba87c630de68dd5b1606180897a2
Travis Maynard <hello@travismaynard.com>
2017-11-15 20:53:51 -0500

    Added initial art for a nine slice and fleshed out more of the pet state.

--- Scripts/Pet.gd
+++ Scripts/Pet.gd
@@ -8,23 +8,22 @@ var newDirection = Vector2(0, 0)
 
 func _ready():
 	set_fixed_process(true)
-	
+
 	# Initialize Sprite
 	Sprite = get_node("Sprite")
 	Sprite.set_texture(load(get_texture(2)))
 	Sprite.set_hframes(2)
-	
+
 	# Random Pet Movement
 	Timer = get_node("Timer")
 	Timer.connect("timeout", self, "set_direction")
-	
 
 func _fixed_process(delta):
 	var motion
 	var direction = Vector2(0, 0)
-	
+
 	direction += newDirection
-	
+
 	motion = direction.normalized() * speed * delta
 	motion = move(motion)
 
--- Scripts/PetState.gd
+++ Scripts/PetState.gd
@@ -1,8 +1,38 @@
 extends Node
 
+var age = 0;
 var hunger = 10
 var hygiene = 10
 var happiness = 10
 
 func _ready():
-	pass
+	set_process(true)
+
+func _process(delta):
+    # Add Timer To Decrease Stats Over Timer
+
+    # Add Timer To Age The Pet (1 Day = 10 Minutes?)
+    pass
+
+func feed_pet(type):
+    if (hunger == 10):
+        return
+
+    if (type == 'snack'):
+        hunger = hunger + 1;
+        # Spend One Intensity Point
+    elif (type == 'meal' and hunger <= 8):
+        hunger = hunger + 2
+        # Spend Two Intensity Points
+
+func bathe_pet():
+    if (hygiene == 10):
+        return
+
+    hygiene = hygiene + 1
+
+func entertain_pet():
+    if (happiness == 10):
+        return
+
+    happiness = happiness + 1
