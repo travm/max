8cca02017aa5fd1ecb78e9062d61e965f5925efd
Travis Maynard <hello@travismaynard.com>
2017-11-22 15:55:55 -0500

    Updated HUD art and other tweaks.

--- Scripts/HUD.gd
+++ Scripts/HUD.gd
@@ -3,11 +3,14 @@ extends CanvasLayer
 var GlobalState
 
 var UnitsLabel
+var IntensityLabel
 
 func _ready():
 	set_process(true)
 	GlobalState = get_node("/root/GlobalState")
 	UnitsLabel = get_node("Units")
+	IntensityLabel = get_node("Intensity")
 
 func _process(delta):
 	UnitsLabel.set_text(str(GlobalState.get_units()))
+	IntensityLabel.set_text(str(GlobalState.get_intensity()))
