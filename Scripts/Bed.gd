f8949d206be062b936ea56a3ea189514c5d34b04
Travis Maynard <hello@travismaynard.com>
2017-11-23 01:29:50 -0500

    Refactored walls to use a new perspective.

--- Scripts/Bed.gd
+++ Scripts/Bed.gd
@@ -3,15 +3,24 @@ extends StaticBody2D
 var Save
 
 var RayCast
+var SaveSprite
 
 func _ready():
 	set_process_input(true)
+	set_process(true)
 
 	Save = get_node("/root/Save")
 
 	RayCast = get_node("RayCast2D")
+	SaveSprite = get_node("SaveSprite")
 
 func _input(event):
 	if (RayCast.is_colliding() and event.is_action_pressed("ui_accept")):
 		print("Saving!")
 		Save.save_game()
+
+func _process(delta):
+	if (RayCast.is_colliding()):
+		SaveSprite.set_hidden(false)
+	else:
+		SaveSprite.set_hidden(true)
